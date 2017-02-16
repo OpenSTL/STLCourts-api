07b6b6237ab025ec9e3fc6ad11462f5243a4642f
Alex White <awhite@rgare.com>
2017-02-16 08:52:03 -0600

    [Multiple Data Sources] Make factory final (just needed to commit)

--- src/main/java/svc/data/citations/CitationDataSourceFactory.java
+++ src/main/java/svc/data/citations/CitationDataSourceFactory.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringJoiner;
 
-public class CitationDataSourceFactory extends BaseJdbcDao {
+public final class CitationDataSourceFactory extends BaseJdbcDao {
     private static final CitationDataSource tylerSource = new TylerCitationDataSource();
 
     public static List<CitationDataSource> getAllCitationDataSources() {
