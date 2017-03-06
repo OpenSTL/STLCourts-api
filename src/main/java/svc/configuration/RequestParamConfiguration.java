bdc6b1fb284edd636842b18f817aa9e0c35ea2b6
ctwoolsey <clark.woolsey@gmail.com>
2017-03-06 11:43:27 -0600

    fixed Hashing for multiple municipalities

--- src/main/java/svc/configuration/RequestParamConfiguration.java
+++ src/main/java/svc/configuration/RequestParamConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import svc.converter.LocalDateConverter;
+import svc.converter.LocalDateTimeConverter;
 
 @Configuration
 @EnableWebMvc
@@ -13,5 +14,6 @@ public class RequestParamConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addFormatters(FormatterRegistry registry){
 		registry.addConverter(new LocalDateConverter("yyyy-MM-dd"));
+		registry.addConverter(new LocalDateTimeConverter("yyyy-MM-dd'T'HH:mm:ss.SSS"));
 	}
 }
--- src/main/java/svc/controllers/CitationController.java
+++ src/main/java/svc/controllers/CitationController.java
@@ -1,6 +1,7 @@
 package svc.controllers;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -18,6 +19,7 @@ import svc.dto.CitationsDTO;
 import svc.logging.LogSystem;
 import svc.managers.*;
 import svc.models.*;
+import svc.security.HashUtil;
 
 @RestController
 @EnableAutoConfiguration
@@ -25,6 +27,9 @@ import svc.models.*;
 public class CitationController {	
 	@Inject
 	CitationManager citationManager;
+	
+	@Inject
+	HashUtil hashUtil;
 	
 	@ResponseBody
 	@RequestMapping(method = RequestMethod.GET, value="/{id}")
@@ -38,12 +43,13 @@ public class CitationController {
 	
 	@ResponseBody
 	@RequestMapping(method = RequestMethod.GET)
+	//DOB parameter must be an ISO dateString of format 'yyyy-MM-dd'  no Time on end of string
 	CitationsDTO FindCitations(@RequestParam(value = "citationNumber", required = false) String citationNumber,
 			                     @RequestParam(value = "licenseNumber", required = false) String licenseNumber,
 			                     @RequestParam(value = "licenseState", required = false) String licenseState,
 			                     @RequestParam(value = "firstName", required = false) String firstName,
 			                     @RequestParam(value = "lastName", required = false) String lastName,
-			                     @RequestParam(value = "municipalityIds", required = false) List<Long> municipalityIds,
+			                     @RequestParam(value = "municipalityIds", required = false) List<String> municipalityIds,
 			                     @RequestParam(value = "dob", required = false) LocalDate dob) {
 		CitationSearchCriteria criteria = new CitationSearchCriteria();
 		if (citationNumber != null) {
@@ -61,7 +67,10 @@ public class CitationController {
 		
 		if (lastName != null && municipalityIds != null && municipalityIds.size() != 0) {
 			criteria.lastName = lastName;
-			criteria.municipalities = municipalityIds;
+			criteria.municipalities = new ArrayList<Long>();
+			for(String municipalityId : municipalityIds){
+				criteria.municipalities.add(hashUtil.decode(Municipality.class,municipalityId));
+			}
 		}
 		
 		if (firstName != null && lastName != null &&  licenseNumber != null) { //for the text/phone system
