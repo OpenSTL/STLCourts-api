90edfc1da718db48689866c5c15dcc3c176d50d3
ctwoolsey <clark.woolsey@gmail.com>
2017-03-10 17:19:11 -0600

    Added response headers to CORS filter

--- src/main/java/svc/SimpleCORSFilter.java
+++ src/main/java/svc/SimpleCORSFilter.java
@@ -18,7 +18,7 @@ public class SimpleCORSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type, x-requested-with, Cache-Control");
 		chain.doFilter(req, res);
 	}
 
