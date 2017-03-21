c158d18f556057b994f3d21bfe0ac42bf70da775
Will Stampley <will.stampley@gmail.com>
2017-03-20 21:02:24 -0500

    Maven conversion for Hashids

--- src/main/java/svc/security/HashUtil.java
+++ src/main/java/svc/security/HashUtil.java
@@ -1,5 +1,6 @@
 package svc.security;
 
+import org.hashids.Hashids;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
--- src/test/java/svc/security/HashUtilTest.java
+++ src/test/java/svc/security/HashUtilTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import org.hashids.Hashids;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
