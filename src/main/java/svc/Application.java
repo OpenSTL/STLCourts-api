d50c4f81662c7d89fc7240a2f497b24df0e84269
ctwoolsey <clark.woolsey@gmail.com>
2017-03-29 17:45:09 -0500

    Added caching support for Court methods

--- src/main/java/svc/Application.java
+++ src/main/java/svc/Application.java
@@ -2,6 +2,7 @@ package svc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@EnableCaching
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/svc/managers/CourtManager.java
+++ src/main/java/svc/managers/CourtManager.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import svc.data.municipal.CourtDAO;
@@ -14,14 +15,17 @@ public class CourtManager {
 	@Inject
 	private CourtDAO courtDAO;
 		
+	@Cacheable(value="courtsById")
 	public Court getCourtById(Long courtId){
 		return courtDAO.getCourtById(courtId);
 	}
 
+	@Cacheable(value="courtsByMunicipalityId")
 	public List<Court> getCourtsByMunicipalityId(Long municipalityId) {
 		return courtDAO.getCourtsByMunicipalityId(municipalityId);
 	}
 
+	@Cacheable(value="allCourts")
 	public List<Court> getAllCourts() {
 		return courtDAO.getAllCourts();
 	}
