ca36cd0574d88b0895236b3dcc000e6e0ebc1ced
Alex White <awhite@rgare.com>
2017-03-30 08:05:02 -0500

    Fix changelog issues with mysql and existing state

--- src/main/resources/sql/changelog/2017-03-23_SMSAlert-table-modifications.sql
+++ src/main/resources/sql/changelog/2017-03-23_SMSAlert-table-modifications.sql
@@ -2,15 +2,15 @@
 
 CREATE TABLE sms_alerts
 (
-	id 						INTEGER 		NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    citation_number 		VARCHAR(25),
-    court_date 				DATETIME,
-    phone_number			VARCHAR(25),
-    date_of_birth 			DATE     						NULL
+  id 						      INTEGER 		        NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  citation_number 		VARCHAR(25),
+  court_date 				  DATETIME,
+  phone_number			  VARCHAR(25),
+  date_of_birth 			DATE     						NULL
 )ENGINE=InnoDB;
 
 # Add auto_increment to violations
-ALTER TABLE violations MODIFY COLUMN INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE violations MODIFY id INTEGER NOT NULL AUTO_INCREMENT;
 
 # Add auto_increment to citations
-ALTER TABLE citations MODIFY COLUMN INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY;
+ALTER TABLE citations MODIFY id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY;
