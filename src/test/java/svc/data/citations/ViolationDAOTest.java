3d2a980011eabd2964f76f270667c852f33e569c
ctwoolsey <clark.woolsey@gmail.com>
2017-04-01 23:38:37 -0500

    Created placeholder test for ViolationDAOTest so unit tests don't fail.

--- src/test/java/svc/data/citations/ViolationDAOTest.java
+++ src/test/java/svc/data/citations/ViolationDAOTest.java
@@ -1,9 +1,17 @@
 package svc.data.citations;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ViolationDAOTest {
     
+	@Test
+	public void TODOTest(){
+		assertThat(true, is(true));
+	}
 }
