15123db38040c23368356ea80aa4314ff90b477a
ctwoolsey <clark.woolsey@gmail.com>
2017-04-02 18:01:05 -0500

    Updated Yaml for texting updated insert-citation to reflect citation structure.

--- src/main/resources/sql/citation/insert-citation.sql
+++ src/main/resources/sql/citation/insert-citation.sql
@@ -1,4 +1,4 @@
 INSERT INTO citations 
-(citation_number,citation_date,first_name,last_name,date_of_birth,defendant_address,defendant_city,defendant_state,drivers_license_number,court_date,court_location,court_address,court_id)
+(citation_number,citation_date,first_name,last_name,date_of_birth,defendant_address,defendant_city,defendant_state,drivers_license_number,court_date,court_id)
 VALUES 
-(:citationNumber,:citationDate,:firstName,:lastName,:dob,:address,:city,:state,:driversLicenseNumber,:courtDate,:courtLocation,:courtAddress,:courtId)
+(:citationNumber,:citationDate,:firstName,:lastName,:dob,:address,:city,:state,:driversLicenseNumber,:courtDate,:courtId)
