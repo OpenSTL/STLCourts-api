7e17ded895c5a303bce21b85c282454719a3a8ac
ctwoolsey <clark.woolsey@gmail.com>
2017-05-03 11:22:15 -0500

    Added GreenPark, Norwood Court, Marlbourough, Twin Oaks, and WIlber Park to county support. (#49)

---
+++ src/main/resources/sql/changelog/2017-05-02_Add_Supported_Municipalities.sql
@@ -0,0 +1,15 @@
+UPDATE municipality
+SET payment_url="https://www.municipalonlinepayments.com/stlouiscountymo/court/search"
+WHERE municipality_id = 86 OR municipality_id = 74 OR municipality_id = 51 OR municipality_id = 56 OR municipality_id = 38;
+
+INSERT INTO citation_datasource_municipality (citation_datasource_id,municipality_id) VALUES (1,86);
+INSERT INTO citation_datasource_municipality (citation_datasource_id,municipality_id) VALUES (1,74);
+INSERT INTO citation_datasource_municipality (citation_datasource_id,municipality_id) VALUES (1,51);
+INSERT INTO citation_datasource_municipality (citation_datasource_id,municipality_id) VALUES (1,56);
+INSERT INTO citation_datasource_municipality (citation_datasource_id,municipality_id) VALUES (1,38);
+
+UPDATE municipality_court
+SET court_id=35
+WHERE municipality_id = 51;
+
+DELETE FROM court WHERE court_id=47;
