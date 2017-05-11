82da167b1ed203c538b985352dd324c5a8c8878e
ctwoolsey <clark.woolsey@gmail.com>
2017-05-11 14:45:18 -0500

    Changed name of County Courts

---
+++ src/main/resources/sql/changelog/2017-05-10_Rename_County_Courts.sql
@@ -0,0 +1,15 @@
+UPDATE court
+SET court_name="St. Louis County Municipal Court - West"
+WHERE court_id = 80;
+
+UPDATE court
+SET court_name="St. Louis County Municipal Court - Central"
+WHERE court_id = 79;
+
+UPDATE court
+SET court_name="St. Louis County Municipal Court - North"
+WHERE court_id = 51;
+
+UPDATE court
+SET court_name="St. Louis County Municipal Court - South"
+WHERE court_id = 35;
