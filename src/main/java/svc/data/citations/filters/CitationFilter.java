57a41c890ba90c4046b6f0b7b6866965b63776ac
ctwoolsey <clark.woolsey@gmail.com>
2017-05-10 20:15:04 -0500

    Modified citationFilter to use streams

--- src/main/java/svc/data/citations/filters/CitationFilter.java
+++ src/main/java/svc/data/citations/filters/CitationFilter.java
@@ -3,6 +3,7 @@ package svc.data.citations.filters;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
@@ -19,28 +20,37 @@ public class CitationFilter {
 	CourtManager courtManager;
 
 	public List<Citation> RemoveCitationsWithExpiredDates(List<Citation> citations){
-		ListIterator<Citation> iter = citations.listIterator();
-		while(iter.hasNext()){
-			Citation citation = iter.next();
-			boolean hasWarrant = false;
-			ListIterator<Violation> violIter = citation.violations.listIterator();
-			while(violIter.hasNext()){
-				Violation violation = violIter.next();
-				if (violation.warrant_status){
-					hasWarrant = true;
-					break;
-				}
-			}
-			if (!hasWarrant){
-				Court court = courtManager.getCourtById(citation.court_id.getValue());
-				if (court.citation_expires_after_days >= 0){
-					LocalDateTime today = LocalDateTime.now();
-					if (citation.court_dateTime.isBefore(today.minusDays(court.citation_expires_after_days))){
-						iter.remove();
-					}
-				}
+		return citations.stream()
+				 .filter(c -> courtDateFilter(c))
+				 .collect(Collectors.toList());
+	}
+	
+	private boolean courtDateFilter(Citation citation){
+		boolean keepCitation = false;
+		
+		ListIterator<Violation> violIter = citation.violations.listIterator();
+		while(violIter.hasNext()){
+			Violation violation = violIter.next();
+			if (violation.warrant_status){
+				keepCitation = true;
+				break;
 			}
 		}
-		return citations;
+		
+		if (!keepCitation){
+			Court court = courtManager.getCourtById(citation.court_id.getValue());
+			if (court.citation_expires_after_days >= 0){
+				LocalDateTime today = LocalDateTime.now();
+				if (citation.court_dateTime.isBefore(today.minusDays(court.citation_expires_after_days))){
+					keepCitation = false;
+				}else{ //citation has not expired yet
+					keepCitation = true;
+				}
+			}else{ //court field indicates citation never expires
+				keepCitation = true;
+			}
+		}
+		
+		return keepCitation;
 	}
 }
