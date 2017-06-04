7cd4739627e8febfb64c20e7171ebdd93dfbee91
ctwoolsey <clark.woolsey@gmail.com>
2017-06-04 18:40:58 -0500

    Added Court and Division to court names

---
+++ src/main/resources/sql/changelog/2017-06-05_Rename_Courts.sql
@@ -0,0 +1,6 @@
+UPDATE court set court_name = cast ( court_name as nvarchar(50)) + cast (' Court' as nvarchar(50))
+
+UPDATE court set court_name="St. Louis County Municipal Court - West Division" WHERE court_id=80
+UPDATE court set court_name="St. Louis County Municipal Court - Central Division" WHERE court_id=79
+UPDATE court set court_name="St. Louis County Municipal Court - North Division" WHERE court_id=51
+UPDATE court set court_name="St. Louis County Municipal Court - South Division" WHERE court_id=35
