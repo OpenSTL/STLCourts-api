e7e1541bf96a8ae117b3e1c201fed2f565d69612
Alex White <alex@cardinalsoftwaresolutions.com>
2017-06-05 00:16:04 -0500

    Fixed changelog

--- src/main/resources/sql/changelog/2017-06-05_Rename_Courts.sql
+++ src/main/resources/sql/changelog/2017-06-05_Rename_Courts.sql
@@ -1,7 +1,10 @@
-ALTER TABLE court MODIFY court_name VARCHAR(100)
-SELECT CONCAT(court_name, ' Court') court_name FROM court
+ALTER TABLE court MODIFY court_name VARCHAR(100);
 
-UPDATE court set court_name="St. Louis County Municipal Court - West Division" WHERE court_id=80
-UPDATE court set court_name="St. Louis County Municipal Court - Central Division" WHERE court_id=79
-UPDATE court set court_name="St. Louis County Municipal Court - North Division" WHERE court_id=51
-UPDATE court set court_name="St. Louis County Municipal Court - South Division" WHERE court_id=35
+SET SQL_SAFE_UPDATES = 0;
+UPDATE court SET court_name = CONCAT(court_name, ' Court');
+SET SQL_SAFE_UPDATES = 1;
+
+UPDATE court set court_name="St. Louis County Municipal Court - West Division" WHERE court_id=80;
+UPDATE court set court_name="St. Louis County Municipal Court - Central Division" WHERE court_id=79;
+UPDATE court set court_name="St. Louis County Municipal Court - North Division" WHERE court_id=51;
+UPDATE court set court_name="St. Louis County Municipal Court - South Division" WHERE court_id=35;
