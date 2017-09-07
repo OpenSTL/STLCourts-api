b1117e27933a392bc76e00528b25f7d1d41de087
Alex White <alex@cardinalsoftwaresolutions.com>
2017-09-06 23:03:09 -0500

    Fixed changelog sql missing a comma

--- src/main/resources/sql/changelog/2017-08-18_Update-Mackenzie-Shrewsbury-data-and-relationship.sql
+++ src/main/resources/sql/changelog/2017-08-18_Update-Mackenzie-Shrewsbury-data-and-relationship.sql
@@ -1,1 +1,1 @@
-UPDATE court SET court_name = 'Shrewsbury Court', phone = '314.647.8634', extension = '5', website = 'http://www.cityofshrewsbury.com/municipal-courts.htm' payment_system='IPG' WHERE court_id = 44;
+UPDATE court SET court_name = 'Shrewsbury Court', phone = '314.647.8634', extension = '5', website = 'http://www.cityofshrewsbury.com/municipal-courts.htm', payment_system='IPG' WHERE court_id = 44;
