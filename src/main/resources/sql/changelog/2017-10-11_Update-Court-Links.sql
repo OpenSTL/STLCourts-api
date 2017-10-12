1b4ff82c241f224a551ddaddc0f8cd7b3dcf579c
ctwoolsey <clark.woolsey@gmail.com>
2017-10-12 10:11:06 -0500

    Removed the changes that deleted Vinita Terrace Municipality.

--- src/main/resources/sql/changelog/2017-10-11_Update-Court-Links.sql
+++ src/main/resources/sql/changelog/2017-10-11_Update-Court-Links.sql
@@ -77,10 +77,3 @@ UPDATE court SET rights_type = 'LINK', rights_value = 'http://www.stlouisco.com/
 UPDATE court SET rights_type = 'LINK', rights_value = 'http://www.stlouisco.com/LawandPublicSafety/MunicipalCourts/NoticeofRights' WHERE court_id = 80
 UPDATE court SET rights_type = 'LINK', rights_value = 'http://www.stlouisco.com/LawandPublicSafety/MunicipalCourts/NoticeofRights' WHERE court_id = 51
 UPDATE court SET rights_type = 'LINK', rights_value = 'http://www.stlouisco.com/LawandPublicSafety/MunicipalCourts/NoticeofRights' WHERE court_id = 35
-
-SET FOREIGN_KEY_CHECKS=0;
-DELETE FROM court WHERE court_id=72;
-DELETE FROM municipality WHERE municipality_id=81;
-DELETE FROM municipality_court WHERE municipality_id = 81 AND court_id = 72;
-DELETE FROM judges WHERE court_id=72;
-SET FOREIGN_KEY_CHECKS=1;
