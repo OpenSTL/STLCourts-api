d35adf70b3a437f2f044e2d468a0e66eb29f3dd7
ctwoolsey <clark.woolsey@gmail.com>
2017-10-13 09:22:38 -0500

    Updated changelog order to satisfy FK Constraints

--- src/main/resources/sql/changelog/2017-10-12_Remove_Vinita_Terrace_Municipality.sql
+++ src/main/resources/sql/changelog/2017-10-12_Remove_Vinita_Terrace_Municipality.sql
@@ -1,6 +1,4 @@
-SET FOREIGN_KEY_CHECKS=0;
+DELETE FROM municipality_court WHERE municipality_id = 81 AND court_id = 72;
 DELETE FROM court WHERE court_id=72;
 DELETE FROM municipality WHERE municipality_id=81;
-DELETE FROM municipality_court WHERE municipality_id = 81 AND court_id = 72;
 DELETE FROM judges WHERE court_id=72;
-SET FOREIGN_KEY_CHECKS=1;
