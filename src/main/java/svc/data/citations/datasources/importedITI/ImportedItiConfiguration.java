834ef57cce06818f3d1db9126dd97935f3d9dd9c
ctwoolsey <clark.woolsey@gmail.com>
2018-02-05 11:54:43 -0600

    Added apiKey in ITI header

--- src/main/java/svc/data/citations/datasources/importedITI/ImportedItiConfiguration.java
+++ src/main/java/svc/data/citations/datasources/importedITI/ImportedItiConfiguration.java
@@ -8,4 +8,7 @@ public class ImportedItiConfiguration {
 
 	@Value("${stlcourts.citationDataSources.importedITI.rootUrl}")
 	public String rootUrl;
+	
+	@Value("${stlcourts.citationDataSources.importedITI.apiKey}")
+	public String apiKey;
 }
