ec7f742a7b98f5efc1bfc3661b13a16467f722c3
ctwoolsey <clark.woolsey@gmail.com>
2018-05-22 12:11:00 -0500

    Added Ballwin to Rejis

---
+++ src/main/resources/sql/changelog/2018-05-22_Add_Ballwin.sql
@@ -0,0 +1,3 @@
+INSERT INTO citation_datasource_municipality (citation_datasource_id,municipality_id) VALUES (2,1); #Clayton
+INSERT INTO datasource_municipality_mapping (citation_datasource_id,municipality_id,datasource_municipality_identifier) VALUES (2,1,'BW'); #Ballwin
+INSERT INTO rejis_court_mapping (court_id,rejis_court_identifier) VALUES (1,'BW'); #Ballwin
